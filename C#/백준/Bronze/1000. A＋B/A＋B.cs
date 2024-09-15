84a528283bdf9c72d59f1aed7b205492ca608099
mosam6357 <irxtcy65@gmail.com>
2024-09-15 13:22:09 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace affsawedgs{
+    class edgwe{
+        static void Main(){
+            string[] aa = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(aa[0])+int.Parse(aa[1]));
+        }
+    }
+}
