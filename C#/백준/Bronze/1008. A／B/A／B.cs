ec7c7744fe2d9e1b6dc45150e4d95d6178528b2e
mosam6357 <irxtcy65@gmail.com>
2024-09-15 13:39:54 +0900

    [Bronze V] Title: A/B, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ C#/백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            string[] aa = Console.ReadLine().Split();
+            double a = double.Parse(aa[0]);
+            double b = double.Parse(aa[1]);
+            double c = a/b;
+            Console.WriteLine(c);
+        }
+    }
+}
