8357adb14c9e727eac8783bb6866edee2858baea
mosam6357 <irxtcy65@gmail.com>
2024-09-16 11:52:06 +0900

    [Bronze V] Title: 웰컴, Time: 44 ms, Memory: 5308 KB -BaekjoonHub

---
+++ C#/백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            Console.WriteLine(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
+        }
+    }
+}
