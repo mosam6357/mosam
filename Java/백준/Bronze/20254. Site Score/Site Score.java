b34d11038f3c6150da594e0dc7a3942cd2cd858d
mosam6357 <irxtcy65@gmail.com>
2024-09-16 11:58:55 +0900

    [Bronze V] Title: Site Score, Time: 176 ms, Memory: 17656 KB -BaekjoonHub

---
+++ Java/백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextInt()*56+in.nextInt()*24+in.nextInt()*14+in.nextInt()*6);
+    }
+}
