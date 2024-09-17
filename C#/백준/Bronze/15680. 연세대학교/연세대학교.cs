2bfdb0eef7373ad000a54691ee650a1934e78366
mosam6357 <irxtcy65@gmail.com>
2024-09-17 13:05:21 +0900

    [Bronze V] Title: 연세대학교, Time: 48 ms, Memory: 5444 KB -BaekjoonHub

---
+++ C#/백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            if(a==0){
+                Console.WriteLine("YONSEI");
+            }
+            else{
+                Console.WriteLine("Leading the Way to the Future");
+            }
+        }
+    }
+}
