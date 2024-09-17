4ea0a859c0ae9b0a899b17fc8083a57b107e8c19
mosam6357 <irxtcy65@gmail.com>
2024-09-17 13:18:05 +0900

    [Bronze V] Title: 연세대학교, Time: 168 ms, Memory: 17184 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if(a==0){
+            System.out.println("YONSEI");
+        }
+        else{
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
