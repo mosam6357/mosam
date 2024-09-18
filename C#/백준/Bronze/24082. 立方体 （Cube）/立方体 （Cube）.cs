e5ce90b313daa46276c8664ba5cc3fd623735439
mosam6357 <irxtcy65@gmail.com>
2024-09-18 10:51:27 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ C#/백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*a*a);
+        }
+    }
+}
