d7e219603263237bb026567ac28327e5c8f97e7d
mosam6357 <irxtcy65@gmail.com>
2024-09-18 11:05:31 +0900

    [Bronze V] Title: 제리와 톰, Time: 48 ms, Memory: 5520 KB -BaekjoonHub

---
+++ C#/백준/Bronze/16430. 제리와 톰/제리와 톰.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+             string[] aa = Console.ReadLine().Split();
+             Console.WriteLine($"{int.Parse(aa[1])-int.Parse(aa[0])} {int.Parse(aa[1])}");
+        }
+    }
+}
