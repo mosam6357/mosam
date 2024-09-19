e9707680101d0a4fa8fef5aac5be7975b0f631cd
mosam6357 <irxtcy65@gmail.com>
2024-09-19 19:50:45 +0900

    [Bronze V] Title: 콜센터, Time: 44 ms, Memory: 5316 KB -BaekjoonHub

---
+++ C#/백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            Console.WriteLine("     /~\\");
+            Console.WriteLine("    ( oo|");
+            Console.WriteLine("    _\\=/_");
+            Console.WriteLine("   /  _  \\");
+            Console.WriteLine("  //|/.\\|\\\\");
+            Console.WriteLine(" ||  \\ /  ||");
+            Console.WriteLine("============");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+        }
+    }
+}
