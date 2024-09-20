494f5f8df546f8f1121f38b690d99e889594a2e8
mosam6357 <irxtcy65@gmail.com>
2024-09-20 09:30:49 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 44 ms, Memory: 5456 KB -BaekjoonHub

---
+++ C#/백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*3+b*4+c*5);
+        }
+    }
+}
