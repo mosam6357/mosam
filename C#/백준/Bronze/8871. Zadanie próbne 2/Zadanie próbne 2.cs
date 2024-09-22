e190c3e5b344b7665ccb11480411dcb90e483588
mosam6357 <irxtcy65@gmail.com>
2024-09-22 16:06:52 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 48 ms, Memory: 5520 KB -BaekjoonHub

---
+++ C#/백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine())+1;
+            Console.WriteLine($"{a*2} {a*3}");
+        }
+    }
+}
