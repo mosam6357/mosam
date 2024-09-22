0a0f452537f139dfb6b0252066a706fabf08f7e4
mosam6357 <irxtcy65@gmail.com>
2024-09-22 16:22:08 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 304 ms, Memory: 7888 KB -BaekjoonHub

--- C#/백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cs
+++ C#/백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cs
@@ -4,24 +4,19 @@ namespace q{
     class w{
         static void Main(){
             string[] aa = Console.ReadLine().Split();
-            int a = int.Parse(aa[0]);
-            int b = int.Parse(aa[1]);
-            int[,] bb = new int[a,b];
-            for (int i=0; i<a; i++){
-                string[] cc = Console.ReadLine().Split();
-                for(int j =0; j<b; j++){
-                    bb[i,j] = int.Parse(cc[j]);
+            int a = int.Parse(aa[0]); int b = int.Parse(aa[1]);
+            int[,] ans= new int[a,b];
+            for(int i =0; i<2; i++){
+                for(int j =0; j<a; j++){
+                    string[] bb = Console.ReadLine().Split();
+                    for(int k=0; k<b; k++){
+                        ans[j,k]+=int.Parse(bb[k]);
+                    }
                 }
             }
-            for (int i=0; i<a; i++){
-                string[] cc = Console.ReadLine().Split();
-                for(int j =0; j<b; j++){
-                    bb[i,j] += int.Parse(cc[j]);
-                }
-            }
-            for (int i =0; i<a; i++){
-                for (int j = 0; j<b; j++){
-                    Console.Write($"{bb[i,j]} ");
+            for(int i=0; i<a; i++){
+                for(int j=0; j<b; j++){
+                    Console.Write(ans[i,j]+" ");                    
                 }
                 Console.WriteLine();
             }
