efb681b82c51972fdd78ea347052233b0c8f66da
mosam6357 <irxtcy65@gmail.com>
2024-09-22 15:47:45 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ Java/백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt()+1;
+        System.out.printf("%d %d",a*2,a*3);
+    }
+}
