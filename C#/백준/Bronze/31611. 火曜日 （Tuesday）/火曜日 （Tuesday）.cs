590cb0c5b34f20f13c93f7beb20f968da0c66ae6
mosam6357 <irxtcy65@gmail.com>
2024-09-23 11:02:28 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ C#/백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            if(int.Parse(Console.ReadLine())%7==2){
+                Console.WriteLine(1);
+            }
+            else{
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
