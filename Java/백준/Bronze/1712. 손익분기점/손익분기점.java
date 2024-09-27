62588446088ddac45eb9a571947c42b4fa7492b5
mosam6357 <irxtcy65@gmail.com>
2024-09-27 12:39:48 +0900

    [Bronze II] Title: 손익분기점, Time: 164 ms, Memory: 17708 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(); int b = in.nextInt(); int c = in.nextInt();
+        if(b>=c){
+            System.out.println(-1);
+        }
+        else{
+            System.out.println(a/(c-b)+1);
+        }
+    }
+}
