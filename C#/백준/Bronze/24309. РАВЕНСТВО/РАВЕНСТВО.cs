6c1909d5f743f9465b7322050acf548029f50cdd
mosam6357 <irxtcy65@gmail.com>
2024-09-28 17:51:06 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 48 ms, Memory: 5628 KB -BaekjoonHub

---
+++ C#/백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Numerics;
+
+namespace q{
+    class w{
+        static void Main(){
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            BigInteger b = BigInteger.Parse(Console.ReadLine());
+            BigInteger c = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine((b-c)/a);
+        }
+    }
+}
