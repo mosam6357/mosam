674ceff59abd0ad874bac299156f3690112007ae
mosam6357 <irxtcy65@gmail.com>
2024-09-28 17:59:49 +0900

    [Bronze V] Title: Copier, Time: 160 ms, Memory: 17508 KB -BaekjoonHub

---
+++ Java/백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for(int i =0; i<a; i++){
+            int b = in.nextInt();
+            System.out.printf("%d %d%n",b,b);
+        }
+    }
+}
