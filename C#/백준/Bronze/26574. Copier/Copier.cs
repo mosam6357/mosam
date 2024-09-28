99dace7479fe802145bf660489ceafd04e7cbcc4
mosam6357 <irxtcy65@gmail.com>
2024-09-28 17:49:25 +0900

    [Bronze V] Title: Copier, Time: 48 ms, Memory: 5728 KB -BaekjoonHub

---
+++ C#/백준/Bronze/26574. Copier/Copier.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            for(int i =0; i<a; i++){
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{b} {b}");
+            }
+        }
+    }
+}
