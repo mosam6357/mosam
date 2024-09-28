e5da6d1fcfa62a3dd6669b8f92c35ec62ae63c5d
mosam6357 <irxtcy65@gmail.com>
2024-09-28 17:52:37 +0900

    [Bronze V] Title: A+B, Time: 52 ms, Memory: 5784 KB -BaekjoonHub

---
+++ C#/백준/Bronze/26711. A＋B/A＋B.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Numerics;
+
+namespace q{
+    class w{
+        static void Main(){
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            BigInteger b = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(a+b);
+        }
+    }
+}
