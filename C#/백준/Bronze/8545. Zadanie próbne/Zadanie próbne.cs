2afbe68732cb1e0a3523b39dfe2526f66e75be03
mosam6357 <irxtcy65@gmail.com>
2024-09-28 17:55:31 +0900

    [Bronze V] Title: Zadanie próbne, Time: 44 ms, Memory: 5408 KB -BaekjoonHub

---
+++ C#/백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            string a = Console.ReadLine();
+            Console.WriteLine($"{a[2]}{a[1]}{a[0]}");
+        }
+    }
+}
