2085602c04dca60ec086efb4fc6c89f8c0e11c0a
mosam6357 <irxtcy65@gmail.com>
2024-09-29 15:21:09 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 48 ms, Memory: 5856 KB -BaekjoonHub

---
+++ C#/백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int q = int.Parse(Console.ReadLine());
+            string w = Console.ReadLine();
+            Console.WriteLine(w.Substring(q-5));
+        }
+    }
+}
