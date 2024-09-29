54013ab14ffce65c1df8b19bcd0f4e21af03ce94
mosam6357 <irxtcy65@gmail.com>
2024-09-29 15:10:43 +0900

    [Bronze IV] Title: 베라의 패션, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ C#/백준/Bronze/15439. 베라의 패션/베라의 패션.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int q = int.Parse(Console.ReadLine());
+            Console.WriteLine(q*(q-1));
+        }
+    }
+}
