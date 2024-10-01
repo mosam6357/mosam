118b5f80eca002c08b460266398de19c2234f1b1
mosam6357 <irxtcy65@gmail.com>
2024-10-01 15:22:55 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/9012. 괄호/괄호.py
+++ Python/백준/Silver/9012. 괄호/괄호.py
@@ -1,16 +1,13 @@
 a=int(input())
 for i in range(a):
     b=input(); ck=0
-    if(b[0]==')' or b[len(b)-1]=='('):
-        ck=1
-    else:
-        for i in range(1,len(b)-1):
-            if(b[i]==')'):
-                if(b[:i].count('(') == b[:i].count(')')):
-                    ck=1
-                    break
-        if(b.count('(')!=b.count(')')):
-            ck=1
+    for j in range(len(b)):
+        if(b[j]=='('):
+            ck+=1
+        else:
+            ck-=1
+            if(ck==-1):
+                break
     if(ck==0):
         print("YES")
     else:
