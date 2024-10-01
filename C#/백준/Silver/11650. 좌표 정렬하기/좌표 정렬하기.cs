d48c345ff9ffee5a4f5f2d37c0bacb1a938a7916
mosam6357 <irxtcy65@gmail.com>
2024-10-01 16:21:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 332 ms, Memory: 28600 KB -BaekjoonHub

---
+++ C#/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace q{
+    class w{
+        static void Main(string[] args){
+            StringBuilder sb = new StringBuilder();
+            int a = int.Parse(Console.ReadLine());
+            var aa = new List<int[]>();
+            for (int i =0; i<a; i++){
+                string[] q = Console.ReadLine().Split();
+                int[] w = Array.ConvertAll(q,int.Parse);
+                aa.Add(w);
+            }
+            var bb = aa.OrderBy(x=>x[0]).ThenBy(x=>x[1]).ToList();
+            for (int i =0; i<a; i++){
+                sb.Append($"{bb[i][0]} {bb[i][1]}\n");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
