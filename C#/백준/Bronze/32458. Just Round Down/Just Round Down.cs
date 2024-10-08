a1c2cf29c5f73381ec4f1a24008284226c99b936
mosam6357 <irxtcy65@gmail.com>
2024-10-08 14:13:15 +0900

    [Bronze V] Title: Just Round Down, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

---
+++ C#/백준/Bronze/32458. Just Round Down/Just Round Down.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine((int)a);
+        }
+    }
+}
