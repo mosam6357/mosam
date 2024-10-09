6b899e720ace5831823a50cf48bd720bab0f8cea
mosam6357 <irxtcy65@gmail.com>
2024-10-09 11:53:25 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 168 ms, Memory: 17632 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if(a==0){
+            System.out.println("divide by zero");
+        }
+        else{
+            System.out.println("1.00");
+        }
+    }
+}
