39e4902ccc8e060fc60a19e085ccc6247f478d0e
mosam6357 <irxtcy65@gmail.com>
2024-10-09 11:46:21 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 48 ms, Memory: 5456 KB -BaekjoonHub

--- C#/백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.cs
+++ C#/백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.cs
@@ -9,16 +9,7 @@ namespace q{
                 Console.WriteLine("divide by zero");
             }
             else{
-                string[] aa = Console.ReadLine().Split();
-                for (int i =0; i<aa.Length; i++){
-                    sum+=int.Parse(aa[i]);
-                }
-                if(sum==0){
-                    Console.WriteLine("divide by zero");
-                }
-                else{
-                    Console.WriteLine("1.00");
-                }
+                Console.WriteLine("1.00");
             }
         }
     }
