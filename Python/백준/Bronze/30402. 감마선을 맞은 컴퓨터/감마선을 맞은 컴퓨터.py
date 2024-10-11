3e96bb68876bac0e91acbe3695e8fc61f16c33f7
mosam6357 <irxtcy65@gmail.com>
2024-10-11 11:33:53 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for i in range(15):
+    aa=input().split()
+    if 'w' in aa:
+        print("chunbae")
+        break
+    elif 'b' in aa:
+        print("nabi")
+        break
+    elif 'g' in aa:
+        print("yeongcheol")
+        break
