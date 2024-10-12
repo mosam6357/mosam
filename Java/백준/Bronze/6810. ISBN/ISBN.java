5e3107d78e2ff1a0fede27c8609777a2f9ccf56e
mosam6357 <irxtcy65@gmail.com>
2024-10-12 12:54:21 +0900

    [Bronze IV] Title: ISBN, Time: 164 ms, Memory: 17724 KB -BaekjoonHub

---
+++ Java/백준/Bronze/6810. ISBN/ISBN.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a =in.nextInt();
+        int b= in.nextInt();
+        int c =in.nextInt();
+        System.out.printf("The 1-3-sum is %d",91+a+c+b*3);
+    }
+}
