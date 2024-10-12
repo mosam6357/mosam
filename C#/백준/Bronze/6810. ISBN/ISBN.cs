45f7f149bd8db8936a67b11d5e4db94bdaf31785
mosam6357 <irxtcy65@gmail.com>
2024-10-12 12:42:33 +0900

    [Bronze IV] Title: ISBN, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ C#/백준/Bronze/6810. ISBN/ISBN.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a=int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+            int c=int.Parse(Console.ReadLine());
+            Console.WriteLine($"The 1-3-sum is {91+a+b*3+c}");
+        }
+    }
+}
