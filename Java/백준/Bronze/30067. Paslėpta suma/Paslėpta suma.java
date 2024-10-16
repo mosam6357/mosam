c2be0ac248fec1d86e0c9d02d0ea3c8730419768
mosam6357 <irxtcy65@gmail.com>
2024-10-16 21:39:46 +0900

    [Bronze IV] Title: Paslėpta suma, Time: 168 ms, Memory: 17652 KB -BaekjoonHub

---
+++ Java/백준/Bronze/30067. Paslėpta suma/Paslėpta suma.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        for (int i =0; i<10; i++){
+            sum += in.nextInt();
+        }
+        System.out.println(sum/2);
+    }
+}
