f860203419759edc22dc3adaef44b9e68d576906
mosam6357 <irxtcy65@gmail.com>
2024-10-18 16:15:42 +0900

    [Bronze IV] Title: 鉛筆 (Pencils), Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ C#/백준/Bronze/15474. 鉛筆 （Pencils）/鉛筆 （Pencils）.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            string[] aaa = Console.ReadLine().Split();
+            int[] aa = Array.ConvertAll(aaa,int.Parse);
+            int c1 = aa[0]%aa[1]==0?aa[0]/aa[1]*aa[2]:(aa[0]/aa[1]+1)*aa[2];
+            int c2 = aa[0]%aa[3]==0?aa[0]/aa[3]*aa[4]:(aa[0]/aa[3]+1)*aa[4];
+            Console.WriteLine(c1<c2?c1:c2);
+        }
+    }
+}
