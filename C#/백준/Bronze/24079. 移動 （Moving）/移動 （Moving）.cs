d43d5419f4f32bbba675636a33b0080c70a78cf0
mosam6357 <irxtcy65@gmail.com>
2024-10-19 10:58:51 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ C#/백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if(a+b>c){
+                Console.WriteLine(0);
+            }
+            else{
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
