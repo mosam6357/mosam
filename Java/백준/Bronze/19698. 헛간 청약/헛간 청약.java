1c4e59bb991581cbb7ea62eb9524eaa09de57229
mosam6357 <irxtcy65@gmail.com>
2024-10-19 11:09:02 +0900

    [Bronze IV] Title: 헛간 청약, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

---
+++ Java/백준/Bronze/19698. 헛간 청약/헛간 청약.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int r = (b/d)*(c/d);
+        System.out.println(a<r?a:r);
+    }
+}
