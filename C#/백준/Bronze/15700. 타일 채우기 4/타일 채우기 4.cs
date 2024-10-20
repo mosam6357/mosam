227f233a66d1fe91e7cacef57e0e7c274740b70f
mosam6357 <irxtcy65@gmail.com>
2024-10-20 11:13:01 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ C#/백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            long[] aa = Array.ConvertAll(Console.ReadLine().Split(),long.Parse);
+            Console.WriteLine((aa[0]*aa[1])/2);
+        }
+    }
+}
