8029aefd697b738acc1e3f747eb03724434929ba
mosam6357 <irxtcy65@gmail.com>
2024-10-20 10:52:48 +0900

    [Bronze IV] Title: 등교, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Bronze/31962. 등교/등교.py
+++ Python/백준/Bronze/31962. 등교/등교.py
@@ -1,12 +1,10 @@
 a,b = map(int,input().split())
-aa=[]; bb=[]
+aa=[]
 for i in range(a):
     c,d = map(int,input().split())
     if(c+d<=b):
         aa.append(c)
-        bb.append(c+d)
 if(len(aa)==0):
     print(-1)
 else:
-    q = bb.index(max(bb))
-    print(aa[q])
+    print(max(aa))
