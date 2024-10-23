66ff62781389c831c08e1feeef92124d3c115a4d
mosam6357 <irxtcy65@gmail.com>
2024-10-23 21:38:11 +0900

    [Bronze II] Title: 분해합, Time: 1988 ms, Memory: 31252 KB -BaekjoonHub

--- Python/백준/Bronze/2231. 분해합/분해합.py
+++ Python/백준/Bronze/2231. 분해합/분해합.py
@@ -1,15 +1,15 @@
 q=input()
 a=int(q)
 l=len(q)
-aa=[]
+ck=1
 for i in range(int(10**(l-2)),a):
     ans = i
     s=str(i)
     for j in range(len(s)):
         ans+=int(s[j])
     if(ans==a):
-        aa.append(i)
-if(len(aa)==0):
+        print(i)
+        ck=0
+        break
+if(ck==1):
     print(0)
-else:
-    print(min(aa))
