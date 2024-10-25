c30d22564faf5425c9ff7b95a0a769834ae3aac2
mosam6357 <irxtcy65@gmail.com>
2024-10-25 23:50:30 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 204 ms, Memory: 18192 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int q  =in.nextInt();
+        for (int i =0; i<q; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            int min = a<b?a:b;
+            min = min<c?min:c;
+            System.out.println(min);
+        }
+    }
+}
