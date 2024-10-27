d3b1525c103120047ac1842a80ea58855e394d71
mosam6357 <irxtcy65@gmail.com>
2024-10-27 17:11:03 +0900

    [Bronze III] Title: Fence Painting, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/11970. Fence Painting/Fence Painting.py
+++ Python/백준/Bronze/11970. Fence Painting/Fence Painting.py
@@ -1,14 +1,7 @@
 aa = list(map(int,input().split()))
 bb = list(map(int,input().split()))
-cc = [i for i in range(min(aa),max(aa)+1)]
-dd = [i for i in range(min(bb),max(bb)+1)]
-ck=0
-for i in cc:
-    if i in dd:
-        ck=1
-        break
-if(ck==0):
-    print(max(aa)-min(aa)+max(bb)-min(bb))
+if(bb[1]<=aa[0] or aa[1]<=bb[0]):
+    print(max(aa)+max(bb)-(min(aa)+min(bb)))
 else:
-    cc = aa+bb
+    cc=aa+bb
     print(max(cc)-min(cc))
