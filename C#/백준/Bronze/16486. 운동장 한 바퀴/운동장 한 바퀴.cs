cd4f825c9eb38b3b61e085d867722060b0bedbb2
mosam6357 <irxtcy65@gmail.com>
2024-10-27 17:33:22 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ C#/백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(2*a+2*b*3.141592);
+        }
+    }
+}
