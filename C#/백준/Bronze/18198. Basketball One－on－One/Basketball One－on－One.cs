94819f20d464aba2f8a01d7cec52d97559adb637
mosam6357 <irxtcy65@gmail.com>
2024-10-28 11:42:48 +0900

    [Bronze IV] Title: Basketball One-on-One, Time: 48 ms, Memory: 5356 KB -BaekjoonHub

---
+++ C#/백준/Bronze/18198. Basketball One－on－One/Basketball One－on－One.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            string a = Console.ReadLine();
+            Console.WriteLine(a[a.Length-2]);
+        }
+    }
+}
