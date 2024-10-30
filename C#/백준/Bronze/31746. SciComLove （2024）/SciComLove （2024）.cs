b9bd66428989b2e92b3093e2ee2d865dc08a7fa4
mosam6357 <irxtcy65@gmail.com>
2024-10-30 21:34:22 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 48 ms, Memory: 5472 KB, Score: 100 point -BaekjoonHub

---
+++ C#/백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            if(a%2==0){
+                Console.WriteLine("SciComLove");
+            }
+            else{
+                Console.WriteLine("evoLmoCicS");
+            }
+        }
+    }
+}
