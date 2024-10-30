16ae6605b586f2b81848adc0f7921b56ce6bea35
mosam6357 <irxtcy65@gmail.com>
2024-10-30 21:39:18 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 184 ms, Memory: 17676 KB, Score: 100 point -BaekjoonHub

---
+++ Java/백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if(a%2==0){
+            System.out.println("SciComLove");
+        }
+        else{
+            System.out.println("evoLmoCicS");
+        }
+    }
+}
