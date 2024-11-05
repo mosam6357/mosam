08adc137784e8b485c49375cf22036b21ebe7834
mosam6357 <irxtcy65@gmail.com>
2024-11-05 21:51:21 +0900

    [Bronze IV] Title: 바코드 닉네임, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ C#/백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            string b = Console.ReadLine().Replace("I","i");
+            Console.WriteLine(b.Replace("l","L"));
+        }
+    }
+}
