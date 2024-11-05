d86be2037a734a22f1f62816c875162e59e60ace
mosam6357 <irxtcy65@gmail.com>
2024-11-05 21:47:30 +0900

    [Bronze IV] Title: H4x0r, Time: 208 ms, Memory: 20200 KB -BaekjoonHub

---
+++ C#/백준/Bronze/26768. H4x0r/H4x0r.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            string a = Console.ReadLine().Replace("a","4");
+            a = a.Replace("e","3");
+            a = a.Replace("i","1");
+            a = a.Replace("o","0");
+            Console.WriteLine(a.Replace("s","5"));
+        }
+    }
+}
