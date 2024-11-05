4efdd51c011bfc27cf9291212075e43257cf8504
mosam6357 <irxtcy65@gmail.com>
2024-11-05 22:17:05 +0900

    [Bronze IV] Title: H4x0r, Time: 84 ms, Memory: 4916 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/26768. H4x0r/H4x0r.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    for (int i=0; i<s.length(); i++){
+        if(s[i]=='a'){
+            cout << 4;
+        }
+        else if(s[i]=='e'){
+            cout << 3;
+        }
+        else if(s[i]=='i'){
+            cout << 1;
+        }
+        else if(s[i]=='o'){
+            cout << 0;
+        }
+        else if(s[i]=='s'){
+            cout << 5;
+        }
+        else{
+            cout << s[i];
+        }
+    }
+}
