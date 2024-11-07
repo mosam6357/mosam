e1a195f3d4cc8cebea7eb59810930ac3bb979b2c
mosam6357 <irxtcy65@gmail.com>
2024-11-07 12:13:16 +0900

    [Bronze IV] Title: Таблица результатов, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C++11/백준/Bronze/28808. Таблица результатов/Таблица результатов.cc
+++ C++11/백준/Bronze/28808. Таблица результатов/Таблица результатов.cc
@@ -6,7 +6,7 @@ int main(){
     int cnt=0;
     scanf("%d %d",&a,&b);
     for(int i=0; i<a; i++){
-        char c[101];
+        char c[b];
         scanf("%s",c);
         for(int j =0; j<strlen(c); j++){
             if(c[j]=='+'){
