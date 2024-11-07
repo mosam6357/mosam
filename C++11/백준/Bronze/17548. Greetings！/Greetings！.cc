a83826d7239c739746d7c66f8dc4b819aae02a81
mosam6357 <irxtcy65@gmail.com>
2024-11-07 12:08:45 +0900

    [Bronze IV] Title: Greetings!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/17548. Greetings！/Greetings！.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char a[1001];
+    scanf("%s",a);
+    int e = strlen(a)-2;
+    printf("h");
+    for (int i =0; i<e*2; i++){
+        printf("e");
+    }
+    printf("y");
+}
