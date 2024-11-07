da8cb05e53824b8bf77a8c34dd8bdd3777979033
mosam6357 <irxtcy65@gmail.com>
2024-11-07 12:01:39 +0900

    [Bronze IV] Title: Greetings!, Time: 244 ms, Memory: 18948 KB -BaekjoonHub

---
+++ Java/백준/Bronze/17548. Greetings！/Greetings！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        int e = a.length()-2;
+        System.out.printf("h");
+        for (int i =0; i<e*2; i++){
+            System.out.printf("e");
+        }
+        System.out.printf("y");
+    }
+}
