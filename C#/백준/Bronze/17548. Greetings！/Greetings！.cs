258b35a106b94fd39b04a5b0e5a6ccb68d6397a3
mosam6357 <irxtcy65@gmail.com>
2024-11-07 11:56:04 +0900

    [Bronze IV] Title: Greetings!, Time: 104 ms, Memory: 5360 KB -BaekjoonHub

---
+++ C#/백준/Bronze/17548. Greetings！/Greetings！.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            string a = Console.ReadLine();
+            int e = a.Length-2;
+            Console.Write('h');
+            for (int i =0; i<e*2; i++){
+                Console.Write('e');
+            }
+            Console.Write('y');
+        }
+    }
+}
