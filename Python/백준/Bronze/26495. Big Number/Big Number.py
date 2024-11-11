eb583c3d8d15516f10ae6e12d5dfa3329b608ecf
mosam6357 <irxtcy65@gmail.com>
2024-11-11 10:02:13 +0900

    [Bronze IV] Title: Big Number, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/26495. Big Number/Big Number.py
@@ -0,0 +1,22 @@
+a=input()
+for i in a:
+    if i=='0':
+        print('0000\n0  0\n0  0\n0  0\n0000\n')
+    elif i=='1':
+        print('   1\n   1\n   1\n   1\n   1\n')
+    elif i=='2':
+        print('2222\n   2\n2222\n2\n2222\n')
+    elif i=='3':
+        print('3333\n   3\n3333\n   3\n3333\n')
+    elif i=='4':
+        print('4  4\n4  4\n4444\n   4\n   4\n')
+    elif i=='5':
+        print('5555\n5\n5555\n   5\n5555\n')
+    elif i=='6':
+        print('6666\n6\n6666\n6  6\n6666\n')
+    elif i=='7':
+        print('7777\n   7\n   7\n   7\n   7\n')
+    elif i=='8':
+        print('8888\n8  8\n8888\n8  8\n8888\n')
+    else:
+        print('9999\n9  9\n9999\n   9\n   9\n')
