f32a910519e2470ad69f4c2e0c676cbb17733e9a
mosam6357 <irxtcy65@gmail.com>
2024-11-14 23:43:40 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.py
+++ Python/백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.py
@@ -1,18 +1,5 @@
 a,b = map(int,input().split())
-cnt = 0
-while(True):
-    if(cnt%2==0):
-        if(a!=0):
-            a-=1
-        else:
-            break
-    else:
-        if(b!=0):
-            b-=1
-        else:
-            break
-    cnt+=1
-if cnt%2==0:
-    print(cnt-1)
+if a<=b:
+    print(2*a-1)
 else:
-    print(cnt)
+    print(2*b+1)
