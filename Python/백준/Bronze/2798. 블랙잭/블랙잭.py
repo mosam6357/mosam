bcb2e6aa7db146315d6307c18b18a9ae5157d400
mosam6357 <irxtcy65@gmail.com>
2024-11-24 10:25:40 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 37492 KB -BaekjoonHub

--- Python/백준/Bronze/2798. 블랙잭/블랙잭.py
+++ Python/백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,7 +3,7 @@ aa = list(map(int,input().split()))
 ck = 0
 bb = []
 for i in range(n-2):
-    for j in range(i+1,n):
+    for j in range(i+1,n-1):
         for k in range(j+1,n):
             s=aa[i]+aa[j]+aa[k]
             if(s<=a):
