5e610369b5d85fbebb577d0ea01c3301cd70335e
mosam6357 <irxtcy65@gmail.com>
2024-11-24 10:34:09 +0900

    [Silver V] Title: 돌 게임, Time: 52 ms, Memory: 5472 KB -BaekjoonHub

---
+++ C#/백준/Silver/9655. 돌 게임/돌 게임.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            if(a%2==1){
+                Console.WriteLine("SK");
+            }
+            else{
+                Console.WriteLine("CY");
+            }
+        }
+    }
+}
