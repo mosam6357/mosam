08cfed05c1d73e7eac3bd2c9fa73683c901d8766
mosam6357 <irxtcy65@gmail.com>
2024-11-26 23:33:06 +0900

    [Bronze III] Title: Sort 마스터 배지훈, Time: 148 ms, Memory: 38100 KB -BaekjoonHub

---
+++ C#/백준/Bronze/17263. Sort 마스터 배지훈/Sort 마스터 배지훈.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int[] aa = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+            Console.WriteLine(aa.Max());
+        }
+    }
+}
