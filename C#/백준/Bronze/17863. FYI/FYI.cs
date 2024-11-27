f2d4a85bf6486fe022ef2ffb4a431d9231346e86
mosam6357 <irxtcy65@gmail.com>
2024-11-27 23:19:16 +0900

    [Bronze IV] Title: FYI, Time: 48 ms, Memory: 5364 KB -BaekjoonHub

---
+++ C#/백준/Bronze/17863. FYI/FYI.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            string a =Console.ReadLine();
+            if(a[0]=='5' && a[1]=='5' && a[2]=='5'){
+                Console.WriteLine("YES");
+            }
+            else{
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
