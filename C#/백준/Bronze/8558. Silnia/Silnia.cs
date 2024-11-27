0686598c897cfe2577f72bc6edb9e77d3e832b65
mosam6357 <irxtcy65@gmail.com>
2024-11-27 23:15:28 +0900

    [Bronze IV] Title: Silnia, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ C#/백준/Bronze/8558. Silnia/Silnia.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int[] aa = {1,1,2,6,4};
+            if(a<5){
+                Console.WriteLine(aa[a]);
+            }
+            else{
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
