2d24417995fad56ac4338a1a507ab6bed59e61a0
mosam6357 <irxtcy65@gmail.com>
2024-12-02 20:27:05 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 48 ms, Memory: 5448 KB -BaekjoonHub

---
+++ C#/백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if(a<=b){
+                Console.WriteLine("high speed rail");
+            }
+            else{
+                Console.WriteLine("flight");
+            }
+        }
+    }
+}
