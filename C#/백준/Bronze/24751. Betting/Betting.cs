ae590c21ac2dc00d148a26713badc2bf2cbb2f09
mosam6357 <irxtcy65@gmail.com>
2024-12-11 11:51:06 +0900

    [Bronze IV] Title: Betting, Time: 52 ms, Memory: 5500 KB -BaekjoonHub

---
+++ C#/백준/Bronze/24751. Betting/Betting.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            float a = float.Parse(Console.ReadLine());
+            float b = 100-a;
+            Console.WriteLine(100/a);
+            Console.WriteLine(100/b);
+        }
+    }
+}
