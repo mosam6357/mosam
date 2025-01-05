ddef27ada0815613e3eb09eacb2615996402a32c
mosam6357 <irxtcy65@gmail.com>
2025-01-05 13:03:38 +0900

    [Bronze III] Title: Desvendando Monty Hall, Time: 52 ms, Memory: 5720 KB -BaekjoonHub

---
+++ C#/백준/Bronze/16445. Desvendando Monty Hall/Desvendando Monty Hall.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            for (int i=0;i<a;i++){
+                int b = int.Parse(Console.ReadLine());
+                if (b!=1){cnt++;}
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
