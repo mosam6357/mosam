2a6c3ffe5175e27526a87c144613452a8fc6720a
mosam6357 <irxtcy65@gmail.com>
2025-01-08 09:25:35 +0900

    [Silver IV] Title: The Big Eye in the Sky, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- Python/백준/Silver/11371. The Big Eye in the Sky/The Big Eye in the Sky.py
+++ Python/백준/Silver/11371. The Big Eye in the Sky/The Big Eye in the Sky.py
@@ -2,14 +2,10 @@ import math
 
 angle = 90
 while(True):
-    x,y = map(int,input().split())
-    if (x==0 and y==0):
+    a,b = map(int,input().split())
+    if (a==0 and b==0):
         break
-    if (x==0):
+    if(a==0):
         print(90)
     else:
-        aa = []
-        n = y/x
-        for i in range(90):
-            aa.append(abs(math.tan(i*(math.pi/180))-n))
-        print(aa.index(min(aa)))
+        print(round(180*math.atan(b/a)/math.pi))
