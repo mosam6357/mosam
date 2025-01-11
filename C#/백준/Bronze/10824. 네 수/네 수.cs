d93f5e79e95cb449ab4b5c65db04e36e79a2bfe5
mosam6357 <irxtcy65@gmail.com>
2025-01-11 10:20:34 +0900

    [Bronze III] Title: 네 수, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10824. 네 수/네 수.cs
@@ -0,0 +1,11 @@
+using System;
+namespace q{
+    class w{
+        static void Main(){
+            string[] aa = Console.ReadLine().Split();
+            long s1 = long.Parse(aa[0]+aa[1]);
+            long s2 = long.Parse(aa[2]+aa[3]);
+            Console.WriteLine(s1+s2);
+        }
+    }
+}
