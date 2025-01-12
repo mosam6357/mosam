3828f51acf36340d722a30293ada21db22937d1e
mosam6357 <irxtcy65@gmail.com>
2025-01-12 21:58:25 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*1000+b*10000);
+        }
+    }
+}
