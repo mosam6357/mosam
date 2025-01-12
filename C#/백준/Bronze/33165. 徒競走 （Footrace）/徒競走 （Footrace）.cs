f7d3e10acfcc4f4d3b3317554506dbaa19cadb8c
mosam6357 <irxtcy65@gmail.com>
2025-01-12 22:02:02 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*b);
+        }
+    }
+}
