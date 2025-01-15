272b6dc27c779c28b949f9643a763699814bd4a6
mosam6357 <irxtcy65@gmail.com>
2025-01-15 10:26:30 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 48 ms, Memory: 5432 KB -BaekjoonHub

---
+++ C#/백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            char c = Console.ReadLine()[0];
+            Console.WriteLine(Math.Abs((int)(c-'I'))+84);
+        }
+    }
+}
