c2e7f2ae4ff99184e7a193595d9cd06b9a5ee544
mosam6357 <irxtcy65@gmail.com>
2025-01-20 10:58:12 +0900

    [Bronze III] Title: GIST 찍기, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Bronze/31907. GIST 찍기/GIST 찍기.py
+++ Python/백준/Bronze/31907. GIST 찍기/GIST 찍기.py
@@ -1,14 +1,15 @@
 a = int(input())
 for i in range(3):
-    if i==0:
-        print('G'*a,end="")
-        print('.'*(a*3))
-    elif i==1:
-        print('.'*a,end="")
-        print('I'*a,end="")
-        print('.'*a,end="")
-        print('T'*a)
-    else:
-        print('.'*(a*2),end="")
-        print('S'*a,end="")
-        print('.'*a)
+    for j in range(a):
+        if i==0:
+            print('G'*a,end="")
+            print('.'*(a*3))
+        elif i==1:
+            print('.'*a,end="")
+            print('I'*a,end="")
+            print('.'*a,end="")
+            print('T'*a)
+        else:
+            print('.'*(a*2),end="")
+            print('S'*a,end="")
+            print('.'*a)
