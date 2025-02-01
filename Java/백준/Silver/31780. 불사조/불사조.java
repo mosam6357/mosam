b7456d9bf252ba25f7e45f57f97f2b5bab7cae4f
mosam6357 <irxtcy65@gmail.com>
2025-02-01 12:36:15 +0900

    [Silver V] Title: 불사조, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ Java/백준/Silver/31780. 불사조/불사조.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        long a = in.nextLong();
+        long b = in.nextLong();
+        System.out.println(a*(b+1));
+    }
+}
