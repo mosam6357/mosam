3dfcafdea9de8bac2e1b29adf4cf0cffb2985851
mosam6357 <irxtcy65@gmail.com>
2025-02-03 14:11:41 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 628 ms, Memory: 6576 KB -BaekjoonHub

---
+++ C#/백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            int a = int.Parse(Console.ReadLine());
+            for (int i=0;i<2*a-1;i++){
+                for (int j=0;j<Math.Abs(a-(i+1));j++){
+                    Console.Write(" ");
+                }
+                for (int j=0;j<a-Math.Abs(a-(i+1));j++){
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
