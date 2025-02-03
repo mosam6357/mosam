2404c625876ff6ce867b9bbc15a815844c9c36fb
mosam6357 <irxtcy65@gmail.com>
2025-02-03 14:28:02 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    for (int i=0;i<2*a-1;i++){
+        for (int j=0;j<abs(a-(i+1));j++){
+            cout << " ";
+        }
+        for (int j=0;j<a-abs(a-(i+1));j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
