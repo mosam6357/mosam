42eb62bb0dd540657fa061f45a5f46311f6ff13f
mosam6357 <irxtcy65@gmail.com>
2025-02-05 09:30:46 +0900

    [Silver V] Title: K번째 수, Time: 1088 ms, Memory: 31204 KB -BaekjoonHub

---
+++ C++11/백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b;
+    cin >> a>> b;
+    int aa[a];
+    for (int i=0;i<a;i++){
+        cin >> aa[i];
+    }
+    stable_sort(aa,aa+a);
+    cout << aa[b-1];
+}
