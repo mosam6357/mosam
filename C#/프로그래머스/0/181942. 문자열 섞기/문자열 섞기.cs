60068ecedad3ce163470e273bcf16393ef37d1c3
mosam6357 <irxtcy65@gmail.com>
2025-02-17 11:29:55 +0900

    [level 0] Title: 문자열 섞기, Time: 0.34 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string str1, string str2) {
+        string answer = "";
+        for (int i=0;i<str1.Length;i++){
+            answer+= str1[i].ToString()+str2[i].ToString();
+        }
+        return answer;
+    }
+}
