fdd3083e64cf2b16e02ff5116329c51d7d8e22f0
mosam6357 <irxtcy65@gmail.com>
2025-02-18 08:44:43 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,13 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if(ineq == "<"):
+        if(eq == "=" and n<=m):
+            answer = 1
+        elif(eq=="!" and n<m):
+            answer = 1
+    else:
+        if(eq=="=" and n>=m):
+            answer = 1
+        elif(eq=="!" and n>m):
+            answer = 1
+    return answer
