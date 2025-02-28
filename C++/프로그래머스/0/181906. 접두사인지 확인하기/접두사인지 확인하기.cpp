e93d550f80df1669bcbb459c201776e27582b274
mosam6357 <irxtcy65@gmail.com>
2025-02-28 12:08:10 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    if(my_string.size()<is_prefix.size()){
+        return 0;
+    }
+    if(my_string.substr(0,is_prefix.size())==is_prefix){
+        return 1;
+    }
+    return 0;
+}
