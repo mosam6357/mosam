e9db6be3cd3ab9006323366ba68f1460c34fb5d8
mosam6357 <irxtcy65@gmail.com>
2025-03-03 13:32:21 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList();
+        for (int i=0;i<todo_list.length;i++){
+            if (finished[i]==false){
+                answer.add(todo_list[i]);
+            }
+        }
+        String[] aa = answer.toArray(new String[answer.size()]);
+        return aa;
+    }
+}
