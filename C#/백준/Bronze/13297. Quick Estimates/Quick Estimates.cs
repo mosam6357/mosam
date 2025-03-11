1133d35f84b21fe4eb4a072680473459544ba0ea
mosam6357 <irxtcy65@gmail.com>
2025-03-11 15:00:44 +0900

    [Bronze IV] Title: Quick Estimates, Time: 52 ms, Memory: 5528 KB -BaekjoonHub

---
+++ C#/백준/Bronze/13297. Quick Estimates/Quick Estimates.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(String[] args){
+            int t = int.Parse(Console.ReadLine());
+            for (int i=0;i<t;i++){
+            string a = Console.ReadLine();
+            Console.WriteLine(a.Length);
+            }
+        }
+    }
+}
