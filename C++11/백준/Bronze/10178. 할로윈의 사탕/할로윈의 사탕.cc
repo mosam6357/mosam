c62097f3de4e83321359c481f0803f51c79da94f
mosam6357 <irxtcy65@gmail.com>
2025-03-12 19:33:37 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int t,a,b;
+    scanf("%d",&t);
+    for (int i=0;i<t;i++){
+        scanf("%d %d",&a,&b);
+        printf("You get %d piece(s) and your dad gets %d piece(s).\n",a/b,a%b);
+    }
+    return 0;
+}
