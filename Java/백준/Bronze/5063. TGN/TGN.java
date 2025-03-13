7b3ecd142a078335544309aceb18da33a3ca75e7
mosam6357 <irxtcy65@gmail.com>
2025-03-13 18:00:47 +0900

    [Bronze III] Title: TGN, Time: 196 ms, Memory: 17636 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5063. TGN/TGN.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            if(a<b-c){
+                System.out.println("advertise");
+            }
+            else if(a==b-c){
+                System.out.println("does not matter");
+            }
+            else{
+                System.out.println("do not advertise");
+            }
+        }
+    }
+}
