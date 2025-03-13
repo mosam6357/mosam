86e9f125d00369f66105920f40aefd7c943af873
mosam6357 <irxtcy65@gmail.com>
2025-03-13 17:46:14 +0900

    [Bronze V] Title: We love kriii, Time: 56 ms, Memory: 5304 KB -BaekjoonHub

---
+++ C#/백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(){
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
