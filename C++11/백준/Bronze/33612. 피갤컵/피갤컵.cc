4145b3174fb3203e7bbf1dc9bb1a911b03814c9f
mosam6357 <irxtcy65@gmail.com>
2025-03-17 13:52:49 +0900

    [Bronze V] Title: 피갤컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/33612. 피갤컵/피갤컵.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a;
+    cin >> a;
+    if(a==1){
+        cout << "2024 8";
+    }
+    else if (a==2){
+        cout << "2025 3";
+    }
+    else if(a==3){
+        cout << "2025 10";
+    }
+    else if(a==4){
+        cout<< "2026 5";
+    }
+    else{
+        cout << "2026 12";
+    }
+    return 0;
+}
