611b221d083484586821b56336ed138ebc885b87
mosam6357 <irxtcy65@gmail.com>
2025-03-17 19:49:20 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 176 ms, Memory: 10492 KB -BaekjoonHub

---
+++ C#/백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+namespace q{
+    class w{
+        static void Main(String[] args){
+            int a = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            for (int i=0;i<a;i++){
+                sb.Append("a");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
