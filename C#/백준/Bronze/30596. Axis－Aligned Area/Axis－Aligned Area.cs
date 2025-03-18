b4934b0a2627f83b0c446f1cf260612dcacc3a71
mosam6357 <irxtcy65@gmail.com>
2025-03-18 13:49:14 +0900

    [Bronze III] Title: Axis-Aligned Area, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ C#/백준/Bronze/30596. Axis－Aligned Area/Axis－Aligned Area.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace q{
+    class w{
+        static void Main(String[] args){
+            int a = int.Parse(Console.ReadLine())+0*int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine())+0*int.Parse(Console.ReadLine());
+            Console.WriteLine(a*b);
+        }
+    }
+}
