cc1bb22e5e576750f8d6f331e47e9c5d17427208
mosam6357 <irxtcy65@gmail.com>
2025-03-18 14:05:44 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 52 ms, Memory: 5664 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+namespace q{
+    class w{
+        static void Main(String[] args){
+            int[] aa = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+            Console.WriteLine(aa.Min());
+        }
+    }
+}
