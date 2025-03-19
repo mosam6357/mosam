4831ca1efbb73206a8b6455a87ce7c7ddb126ee8
mosam6357 <irxtcy65@gmail.com>
2025-03-19 12:17:52 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    string s;
+    cin >> s;
+    cout << ":fan::fan::fan:\n:fan::"<< s <<"::fan:\n:fan::fan::fan:";
+    return 0;
+}
