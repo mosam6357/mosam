8efd096206090c50f3db71029c470016f098e910
mosam6357 <irxtcy65@gmail.com>
2025-03-19 12:05:37 +0900

    [Bronze III] Title: Sum of Odd Sequence, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9713. Sum of Odd Sequence/Sum of Odd Sequence.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i=0;i<a;i++){
+            int n = (in.nextInt()+1)/2;
+            System.out.println(n*n);
+        }
+    }
+}
