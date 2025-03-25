4df136a0ab21b1522281126f3c81ac446e1c3298
mosam6357 <irxtcy65@gmail.com>
2025-03-25 19:54:10 +0900

    [Bronze III] Title: A/B - 3, Time: 124 ms, Memory: 39196 KB, Score: 630 point -BaekjoonHub

--- Python/백준/Bronze/16428. A／B － 3/A／B － 3.py
+++ Python/백준/Bronze/16428. A／B － 3/A／B － 3.py
@@ -1,3 +1,11 @@
 a,b = map(int,input().split())
-print(a//b)
-print(a%b)
+if a==0:
+    print(0)
+    print(0)
+elif b>0 or (b<0 and a%b==0):
+    print(a//b)
+    print(a%b)
+else:
+    q = a//b+1
+    print(q)
+    print(a-q*b)
