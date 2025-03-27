5b61b846450c87cf268a54650a4b24754235f5c7
mosam6357 <irxtcy65@gmail.com>
2025-03-27 15:11:57 +0900

    [Bronze III] Title: 창용이의 시계, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/12840. 창용이의 시계/창용이의 시계.py
+++ Python/백준/Bronze/12840. 창용이의 시계/창용이의 시계.py
@@ -6,10 +6,7 @@ q = int(sys.stdin.readline())
 for i in range(q):
     aa = sys.stdin.readline().rstrip()
     if (aa=='3'):
-        while(t<0):
-            t+=86400
-        while(t>86400):
-            t-=86400
+        t=t%86400
         sys.stdout.write(f"{t//3600} {(t%3600)//60} {t%60}\n")
     else:
         m,n = map(int,aa.split())
