b72a12921272f82659feabf00f4a436a48e433db
mosam6357 <irxtcy65@gmail.com>
2025-03-28 15:20:43 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++14/백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a;
+    cin >> a;
+    for (int i=0;i<a;i++){
+        cout << "Material Management " << i+1 <<endl;
+        cout << "Classification ---- End!" << endl;
+    }
+    return 0;
+}
