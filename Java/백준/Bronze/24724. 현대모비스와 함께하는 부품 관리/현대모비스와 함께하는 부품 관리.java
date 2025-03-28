424ad40a22f2f4d0e3fb9de622be80eca6cad435
mosam6357 <irxtcy65@gmail.com>
2025-03-28 15:14:45 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 180 ms, Memory: 17704 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i=0;i<a;i++){
+            System.out.printf("Material Management %d\n",i+1);
+            System.out.println("Classification ---- End!");
+        }
+    }
+}
